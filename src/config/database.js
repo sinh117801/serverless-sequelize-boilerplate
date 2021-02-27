f74762360e8b646c386438b243749caf242e5be7
Sinh <sinh117801@gmail.com>
2021-02-27 18:36:13 +0700

    update config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
 });
 
 module.exports = sequelize;
